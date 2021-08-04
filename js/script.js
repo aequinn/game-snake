049117b30857f98bb796d548f346c3e34a012123
Anthony Quinn <quinn.ant@gmail.com>
2021-08-03 22:39:08 -0400

    add initial variables for score and gameboard dimension manipulation. restructure of html to have a header and a main.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 /*JavaScript file*/
+//Variable Setup
+let gameboard = [];
+let boardDimension = 20;
+const cellSize = 25;
+
+//Element variables
+const gameboardElement = document.getElementById('gameBoard');
+const openMessageElement = document.getElementById('openMessage')
+
+gameboardElement.style.width = `${boardDimension*cellSize}px`;
+
+//Fill in the gameboard
+for (let i = 0; i < (boardDimension*boardDimension); i++){
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    console.log(cell);
+    gameboard.push(cell);
+    gameboardElement.append(cell);
+    openMessageElement.style.display = 'none';
+
+}
