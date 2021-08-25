92f433fbeb1b0a310845d48e2b2c0f4d6251bf97
Anthony Quinn <quinn.ant@gmail.com>
2021-08-25 09:46:32 -0400

    Add and get snake to appear. Add snake style.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,17 @@ let boardDimension = 20;
 const cellSize = 25;
 let myScore = 0;
 
+let snake = [0,1,2];
+
+
+/*Set and get highscore if it exists */
+let highScore = 0;
+if(localStorage.getItem('highScore')){
+    highScore = localStorage.getItem('highScore');
+}else{
+    console.log('No high score');
+}
+
 //Element variables
 const gameboardElement = document.getElementById('gameBoard');
 const openMessageElement = document.getElementById('openMessage');
@@ -14,8 +25,16 @@ const highScoreElement = document.getElementById('highScore');
 
 setScoreboard();
 buildBoard();
+renderSnake();
 
 /*FUNCTIONS*/
+
+//Render snake
+function renderSnake(){
+    snake.forEach(element => {
+        gameboard[element].classList.add('snake');
+    });
+}
 
 //Function that generates the gameboard
 function buildBoard() {
@@ -37,4 +56,5 @@ function buildBoard() {
 //Function that resets the scoreboard
 function setScoreboard() {
     myScoreElement.textContent = myScore;
+    highScoreElement.textContent = highScore;
 }
